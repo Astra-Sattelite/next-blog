0adece30582f140a66413e1fcc770964b0a617b1
ydrExty <liaroffear@mail.ru>
2021-10-06 16:40:55 +0500

    add getPosts v3

--- src/pages/api/blog.ts
+++ src/pages/api/blog.ts
@@ -1,13 +1,18 @@
 import type { NextApiHandler } from 'next'
+import axios from "axios"
 // import { Post } from "../../features/blog/blogAPI"
 
 const blogHandler: NextApiHandler = async (request, response) => {
-  const {data} = request.body
+
+  const data = axios.get("https://simple-blog-api.crew.red/posts")  
+
+  // request.body
   console.log("HANDLER: " + data)
   // simulate IO latency
   await new Promise((resolve) => setTimeout(resolve, 500))
 
-  response.json({ data: data })
+  // response.json({ data: data })
+  response.json({data: data})
 }
 
 export default blogHandler
