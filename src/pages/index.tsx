da391f9862ab234e4db266ae967f170eb1fb2ba1
ydrExty <liaroffear@mail.ru>
2021-10-06 15:31:28 +0500

    rep init

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+import Counter from '../features/counter/Counter'
+
+const IndexPage: NextPage = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
+}
+
+export default IndexPage
